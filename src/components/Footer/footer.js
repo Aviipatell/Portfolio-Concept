22b0937f68f32b2c40cbcf26789ea827d1c60297
Avi Patel <avi.patel0025@gmail.com>
2022-02-02 15:34:25 -0500

    fixed footer links

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -8,15 +8,20 @@ const Footer = () => {
     <div className="footer">
       <p>
         Designed and developed by{" "}
-        <a className="profileLink" target="_blank" rel="noreferrer" href="http://avipatel.me">
+        <a
+          className="profileLink"
+          target="_blank"
+          rel="noreferrer"
+          href="http://avipatel.me"
+        >
           Avi Patel
         </a>
       </p>
       <div className="footer-links-container">
-        <Link to="/" className="footer-link">
+        <Link to="/Portfolio-Concept" className="footer-link">
           Home
         </Link>
-        <Link to="/work" className="footer-link">
+        <Link to="/Portfolio-Concept/work" className="footer-link">
           Work
         </Link>
       </div>
