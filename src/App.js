9f29347df888556701bec377f4355a340f7e2f41
Avi Patel <avi.patel0025@gmail.com>
2022-02-02 15:16:09 -0500

    Modified routes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
